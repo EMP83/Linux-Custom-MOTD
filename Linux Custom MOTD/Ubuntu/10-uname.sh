f8fe7553dcfd84e0fad45a07085d9cd175c80e22
EMP83 <36104474+EMP83@users.noreply.github.com>
2025-04-10 20:30:46 +0200

    Add files via upload

---
+++ Linux Custom MOTD/Ubuntu/10-uname.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo " "
+echo " "
+#uname -snrvm
+echo " "
+neofetch #install neofetch with sudo apt install neofetch firrst
+echo " "
+echo " "
+#echo "Network:"
+echo " "
+#ip -4 -br a
+#ip -6 -br a
+#ip a | grep "inet"
